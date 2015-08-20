ad12d14be69ea214e49785cee23fc68120d4a9be
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2015-08-20 09:33:38 +0200

    Refactor config form and controllers

--- Model/Config/TNTFranceConfigValue.php
+++ Model/Config/TNTFranceConfigValue.php
@@ -13,15 +13,41 @@
 namespace TNTFrance\Model\Config;
 
 use Thelia\Core\Translation\Translator;
-use TNTFrance\Model\Config\Base\TNTFranceConfigValue as BaseTNTFranceConfigValue;
 use TNTFrance\TNTFrance;
 
 /**
- * Class TNTFranceConfigValue
- * @package TNTFrance\Model\Config
+ * Module configuration values keys.
  */
-class TNTFranceConfigValue extends BaseTNTFranceConfigValue
+class TNTFranceConfigValue
 {
+    const ENABLED = "enabled";
+    const MODE_PRODUCTION = "mode_production";
+    const ACCOUNT_NUMBER = "account_number";
+    const USERNAME = "username";
+    const PASSWORD = "password";
+    const USE_INDIVIDUAL = "use_individual";
+    const USE_ENTERPRISE = "use_enterprise";
+    const USE_DEPOT = "use_depot";
+    const USE_DROPOFFPOINT = "use_dropoffpoint";
+    const PRODUCTS_ENABLED = "products_enabled";
+    const OPTIONS_ENABLED = "options_enabled";
+    const REGULAR_PICKUP = "regular_pickup";
+    const SENDER_NAME = "sender_name";
+    const SENDER_ADDRESS1 = "sender_address1";
+    const SENDER_ADDRESS2 = "sender_address2";
+    const SENDER_ZIP_CODE = "sender_zip_code";
+    const SENDER_CITY = "sender_city";
+    const CONTACT_LASTNAME = "contact_lastname";
+    const CONTACT_FIRSTNAME = "contact_firstname";
+    const CONTACT_EMAIL = "contact_email";
+    const CONTACT_PHONE = "contact_phone";
+    const NOTIFICATION_EMAILS = "notification_emails";
+    const NOTIFICATION_SUCCESS = "notification_success";
+    const LABEL_FORMAT = "label_format";
+    const FREE_SHIPPING = "free_shipping";
+    const MAX_WEIGHT_PACKAGE = "max_weight_package";
+    const TRACKING_URL = "tracking_url";
+
     const SURCHARGE_FUEL = "surcharge_fuel";
     const SURCHARGE_SECURITY_FEE = "surcharge_security_fee";
     const SURCHARGE_MULTI_PACKAGE = "surcharge_multi_package";
@@ -45,22 +71,22 @@ class TNTFranceConfigValue extends BaseTNTFranceConfigValue
         $services[] = [
             'id' => 'INDIVIDUAL',
             'enabled' => (bool) TNTFrance::getConfigValue(TNTFranceConfigValue::USE_INDIVIDUAL),
-            'name' => $translator->trans('Home delivery', [], 'tntfrance'),
+            'name' => $translator->trans('Home delivery', [], TNTFrance::MESSAGE_DOMAIN),
         ];
         $services[] = [
             'id' => 'ENTERPRISE',
             'enabled' => (bool) TNTFrance::getConfigValue(TNTFranceConfigValue::USE_ENTERPRISE),
-            'name' => $translator->trans('Enterprise delivery', [], 'tntfrance'),
+            'name' => $translator->trans('Enterprise delivery', [], TNTFrance::MESSAGE_DOMAIN),
         ];
         $services[] = [
             'id' => 'DEPOT',
             'enabled' => (bool) TNTFrance::getConfigValue(TNTFranceConfigValue::USE_DEPOT),
-            'name' => $translator->trans('TNT Depot', [], 'tntfrance'),
+            'name' => $translator->trans('TNT Depot', [], TNTFrance::MESSAGE_DOMAIN),
         ];
         $services[] = [
             'id' => 'DROPOFFPOINT',
             'enabled' => (bool) TNTFrance::getConfigValue(TNTFranceConfigValue::USE_DROPOFFPOINT),
-            'name' => $translator->trans('Drop Off Point', [], 'tntfrance'),
+            'name' => $translator->trans('Drop Off Point', [], TNTFrance::MESSAGE_DOMAIN),
         ];
 
         return $services;
